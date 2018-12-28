7ffc3fe305b3ee6e15e7a96be24bcef2af6c9873
Ngan Nguyen <better.ngan@1pac.vn>
2018-12-28 14:59:25 +0700

    layout done

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">conduit</router-link>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <!-- Add "active" class when you're on that page" -->
+          <router-link class="nav-link active" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">
+            <i class="ion-compose"></i>&nbsp;New Post
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="">
+            <i class="ion-gear-a"></i>&nbsp;Settings
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="">Sign up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
