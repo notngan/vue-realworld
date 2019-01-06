7b8824445ca0ba6b33827c35623a9e03d138a025
notngan <notngan@gmail.com>
2019-01-06 16:35:35 +0700

    auto login

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,20 @@
 <script>
 import AppHeader from '@/components/Header'
 import AppFooter from '@/components/Footer'
+import { mapActions } from 'vuex'
 
 export default {
   components: {
     AppHeader,
     AppFooter
+  },
+
+  methods: {
+    ...mapActions('auth', ['autoLogin'])
+  },
+
+  created () {
+    this.autoLogin()
   }
 }
 </script>
