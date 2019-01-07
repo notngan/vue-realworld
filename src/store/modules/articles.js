3f9cd2cd5a54d0b904611138f9bbb519d11d10ad
Ngan Nguyen <better.ngan@1pac.vn>
2019-01-07 17:40:34 +0700

    favorite article

--- src/store/modules/articles.js
+++ src/store/modules/articles.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {
-  LOAD_ARTICLES
+  LOAD_ARTICLES,
+  ADD_FAVORITE
 } from '../mutation-types'
 
 const state = {
@@ -10,17 +11,58 @@ const state = {
 const mutations = {
   [LOAD_ARTICLES] (state, payload) {
     state.articleList = payload;
+  },
+
+  [ADD_FAVORITE] (state, payload) {
+    const article = state.articleList.find(art => art.slug == payload)
+    for (let key in article) {
+      if (key == 'favorited') {
+        article[key] = true
+      }
+      if (key == 'favoritesCount') {
+        article[key] += 1
+      }
+    }
   }
 }
 
 const actions = {
-  loadArticles({commit}) {
-    axios.get('articles')
+  loadArticles({ commit }, token) {
+    if (!token) {
+      axios.get('articles')
+        .then(res => commit(LOAD_ARTICLES, res.data.articles))
+        .catch(error => {
+          throw error
+        });
+    } else {
+
+    }
+  },
+
+  loadArticlesByAuthor ({ commit }, username) {
+    axios.get(`articles?author=${username}`)
       .then(res => commit(LOAD_ARTICLES, res.data.articles))
       .catch(error => {
         throw error
-      });
+      })
   },
+
+  addFavorite({ commit }, payload) {
+    axios({
+      method: 'post',
+      url: `articles/${payload.slug}/favorite`,
+      headers: {
+        Authorization: `Token ${payload.token}`
+      }
+    })
+      .then(res => {
+        console.log(res.data.article)
+        commit(ADD_FAVORITE, res.data.article.slug)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
 
 export default {
